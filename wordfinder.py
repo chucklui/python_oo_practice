eb1fb5bccd2da2a6b23c950b109a3d71eec869f2
brendan <lobrendan@gmail.com>
2022-01-25 17:37:22 -0800

    fixed extract_data

--- wordfinder.py
+++ wordfinder.py
@@ -1,15 +1,37 @@
+import random
+import os
+
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
     def __init__(self, location):
+        '''Takes an input location for a file of words, then runs extract_data
+        method to extract the words into self.words'''
+
         self.location = location
         self.words = []
-    
+
+        self.extract_data()
+        print(f"{len(self.words)} words read")
+
+    def __repr__(self):
+        return f"<WordFinder location = {self.location}, words = {self.words}"
+
     def random(self):
-        print
+        '''Returns a random word from the list of words'''
+
+        return random.choice(self.words)
 
     def extract_data(self):
-        words_file = open(self.location, 'r')
-        for word in words_file:
-            self.words.append(word)
-            
+        '''Extracts the words from file at self.location and saves them to
+        self.words. Returns nothing'''
 
+        print(f"In extract_data, location = {self.location}")
+        words_file = open(self.location, "r")
+
+        for word in words_file:
+            print(f"word = {word}")
+            self.words.append(word.strip())
+
+        words_file.close()
