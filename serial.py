29ada2967b78402484c1dd2bb80c00dc55ac7fb2
Chuck Lui <luialex0@gmail.com>
2022-01-25 16:52:11 -0800

    finished serial class

--- serial.py
+++ serial.py
@@ -23,22 +23,19 @@ class SerialGenerator:
 
     def __init__(self, start):
         """this function takes in a start value and set it to the attribute.
-        Have a count attribute set to 0"""
+        Have a next number attribute set to start value"""
 
         self.start = start
-        self.next_num = start + 1
+        self.next_num = start
 
     def generate(self):
         """this function return a integer represent number of times
         this method has been called + start attribute value"""
 
-        if self.start + 1 == self.next_num:
-            return self.start
-        else:
-            self.next_num += 1
-            return self.next_num - 1
+        self.next_num += 1
+        return self.next_num - 1
 
     def reset(self):
-        """this function reset the count back to zero"""
-
-        self.next_num = self.start + 1
+        """this function reset the next number back to start"""
+        
+        self.next_num = self.start
